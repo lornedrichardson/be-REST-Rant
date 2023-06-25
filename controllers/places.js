6be58ac2bf6c2fb97a0c92d90d35950ddd803eb4
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-06-25 13:32:18 -0700

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -17,4 +17,25 @@ router.get('/', (req, res) => {
   res.render('places/index', { places });
 });
 
+router.get('/new', (req, res) => {
+  res.render('places/new');
+});
+
+router.get('/', (req, res) => {
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Las Vegas',
+    state: 'NV',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/table-in-restaurant.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee-cat.jpg'
+  }];
+  res.render('places/index', { places });
+});
+
 module.exports = router;
