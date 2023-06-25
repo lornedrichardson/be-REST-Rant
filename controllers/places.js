9a58fe58b0e9569e3196c8ad17129984b89c75e4
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-06-25 13:54:22 -0700

    Form data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -18,20 +18,8 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Las Vegas',
-    state: 'NV',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/table-in-restaurant.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg'
-  }];
-  res.render('places/index', { places });
+  console.log(req.body);
+  res.send('POST /places');
 });
 
 router.get('/new', (req, res) => {
