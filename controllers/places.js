3de9e45d208f1262eb35d7a16362b29e59a3228a
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-07-02 11:42:25 -0700

    Part 8 finished

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,61 @@
 const router = require('express').Router();
+const db = require('../models');
 
 router.get('/', (req, res) => {
+  db.Place.find()
+  .then((places) => res.render('places/index', { places }))
+  .catch(err => {
+    console.log(err);
+    res.render('error404');
+  });
+});
+
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => res.redirect('/places'))
+  .catch(err => {
+    console.log(err);
+    res.render('error404');
+  });
+});
+
+router.get('/new', (req, res) => {
+  res.render('places/new');
+});
+
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .then(place => res.render('places/show', { place }))
+  .catch(err => {
+    console.log(err);
+    res.render('error404');
+  });
+});
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub');
+});
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub');
+});
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub');
+});
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub');
+});
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub');
+});
+
+module.exports = router;
+
+
+/* router.get('/', (req, res) => {
   let places = [{
     name: 'H-Thai-ML',
     city: 'Las Vegas',
@@ -62,4 +117,4 @@ router.put('/:id', (req, res) => {
   }
 });
 
-module.exports = router;
+module.exports = router; */
