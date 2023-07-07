e3194076e319b0d4cec7ba8cb067e4f13b7be58e
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-07-06 21:32:03 -0700

    Create and use helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,17 +5,26 @@ function show(data) {
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
-            <img src={ data.place.pic } alt={ data.place.name } />
-            <h3>{ data.place.city }, { data.place.state }</h3>
-            <h4>{ data.place.cuisines }</h4>
-            <h5>Rating: currently unrated</h5>
-            <h5>Comments: No comments yet!</h5>
-            <div>
-              <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
-              <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                <button type="submit" className="btn btn-danger">Delete</button>
-              </form>
+            <div className="row">
+              <div className="col-sm-6">
+                <img src={ data.place.pic } alt={ data.place.name } />
+                <h3>Located in { data.place.city }, { data.place.state }</h3>
+              </div>
+              <div className="col-sm-6">
+                <h1>{ data.place.name }</h1>
+                <h2>Rating</h2>
+                <h3>Not Rated</h3>
+                <h2>Description</h2>
+                <h3>{ data.place.showEstablished() }</h3>
+                <h4>Serving { data.place.cuisines }</h4>
+                <h5>Comments: No comments yet!</h5>
+                <div>
+                  <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
+                  <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                    <button type="submit" className="btn btn-danger">Delete</button>
+                  </form>
+                </div>
+              </div>
             </div>
           </main>
         </Def>
