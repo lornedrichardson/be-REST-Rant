0fa95df791e6dbf3d9ab228b1bd86f259d08ea7a
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-07-11 21:45:57 -0700

    Added rating functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,7 +5,15 @@ function show(data) {
   let comments = (
     <h3 className="inactive">No comments yet!</h3>
   )
+  let rating = (
+    <h3 className="inactive">Not yet rated</h3>
+  )
   if(data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => { return tot + c.stars }, 0);
+    let averageRating = sumRatings / data.place.comments.length;
+    rating = (
+      <h3>{ Math.round(averageRating) } stars</h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
@@ -28,7 +36,8 @@ function show(data) {
           <div className="col-sm-6">
             <h1>{ data.place.name }</h1>
             <h2>Rating</h2>
-            <h3>Not Rated</h3>
+            { rating }
+            <br />
             <h2>Description</h2>
             <h3>{ data.place.showEstablished() }</h3>
             <h4>Serving { data.place.cuisines }</h4>
