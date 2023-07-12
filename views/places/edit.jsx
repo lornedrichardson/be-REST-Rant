7e16d071144b767b6d6e0510dfe8c63337bc1fdf
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-07-11 22:06:08 -0700

    be-10-rest-rant-part-11 finish

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -10,23 +10,27 @@ function edit_form(data) {
           <div className="row">
             <div className="form-group col-sm-6">
               <label htmlFor="name">Place Name</label>
-              <input className="form-control" id="name" name="name" value={data.place.name} required />
+              <input className="form-control" id="name" name="name" value={ data.place.name } required />
             </div>
             <div className="form-group col-sm-6">
               <label htmlFor="pic">Place Picture</label>
-              <input className="form-control" type="url" id="pic" name="pic" value={data.place.pic} />
+              <input className="form-control" type="url" id="pic" name="pic" value={ data.place.pic } />
             </div>
             <div className="form-group col-sm-6">
               <label htmlFor="city">Place City</label>
-              <input className="form-control" id="city" name="city" value={data.place.city} />
+              <input className="form-control" id="city" name="city" value={ data.place.city } />
             </div>
             <div className="form-group col-sm-6">
               <label htmlFor="state">Place State</label>
-              <input className="form-control" id="state" name="state" value={data.place.state} />
+              <input className="form-control" id="state" name="state" value={ data.place.state } />
             </div>
             <div className="form-group col-sm-6">
               <label htmlFor="cuisines">Place Cuisines</label>
-              <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
+              <input className="form-control" id="cuisines" name="cuisines" value={ data.place.cuisines } required />
+            </div>
+            <div className="form-group col-sm-6">
+              <label htmlFor="founded">Founded</label>
+              <input className="form-control" id="founded" name="founded" value={ data.place.founded } />
             </div>
           </div>
           <input className="btn btn-primary" type="submit" value="Edit Place" />
